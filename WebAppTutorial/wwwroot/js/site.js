1c317ad043d83c534316f5fbcad6f51966f54995
Heather Shultz <silverlycold@gmail.com>
2022-06-09 22:17:17 -0500

    Add project files.

---
+++ WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿
+// Functions to be called onmouseover and onmouseout to change the color of "Hello World"
+function newColor() {
+    document.getElementById("hello_world").style.color="blue";
+}
+
+function oldColor() {
+    document.getElementById("hello_world").style.color="darkolivegreen";
+}
